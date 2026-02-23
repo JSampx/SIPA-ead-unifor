c224e5f5be22cb9d6a4be6b39f4577c22fb2e8ae
JSampx <joao.sampaio.andrade@gmail.com>
2026-02-23 17:00:54 -0300

    Initial commit

---
+++ streamlit_obs.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# import streamlit as st
+
+# path = "data/olist/" # caminho raiz até os arquivos *.csv
+# orders = pd.read_csv(path + "olist_orders_dataset.csv")
+
+df = pd.DataFrame({
+    "first column": [1, 2, 3, 4, 5, 6],
+    "second column": [7, 8, 9, 10, 11, 12],
+})
